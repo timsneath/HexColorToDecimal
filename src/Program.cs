0bd5ec1edf848dea3cd24d165b85961cf09646bb
Tim Sneath <tims@microsoft.com>
2017-04-14 10:32:23 -0700

    Simple hexcolor to decimal value converter.

---
+++ src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HexColorToDecimal
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Input seven digit hex string, e.g. #FF0000, or press Ctrl+Break to exit.");
+                Console.Write("> ");
+
+                try
+                {
+                    var hexColor = Console.ReadLine().Substring(1);
+
+                    var red = hexColor.Substring(0, 2);
+                    var green = hexColor.Substring(2, 2);
+                    var blue = hexColor.Substring(4, 2);
+
+                    var redValue = Convert.ToInt32(red, 16);
+                    var greenValue = Convert.ToInt32(green, 16);
+                    var blueValue = Convert.ToInt32(blue, 16);
+
+                    Console.WriteLine($"Colors: {redValue}, {greenValue}, {blueValue}\n");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
